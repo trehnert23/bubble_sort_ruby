8385d39e40c57017196bdf06db1937b256d1d1fa
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-17 10:59:05 -0700

    bubble sort finished

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,15 @@
 def bubble_sort(arr)
-  # code in here
+	return arr if arr.size <= 1
+	swapped = true
+
+	while swapped
+	    swapped = false
+	    0.upto(arr.size-2) do |i|
+	      if arr[i] > arr[i+1]
+	        arr[i], arr[i+1] = arr[i+1], arr[i] 
+	        swapped = true
+	      end
+	    end
+	end
+	arr
 end
